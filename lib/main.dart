210f957475e8056cb905588c338475ec7b548941
Rémy LAVERGNE <lavergne.remy@gmail.com>
2020-11-09 23:07:51 +0100

    create username screen with onboarding

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:spotfinder/camera.helper.dart';
+import 'package:spotfinder/screens/onboarding.dart';
 import 'package:spotfinder/views/spot-list.widget.dart';
 import 'package:spotfinder/views/take-picture.dart';
 
@@ -8,31 +9,26 @@ import 'camera.helper.dart';
 void main() async {
   await CameraService.instance.initCameras();
 
-  runApp(MyApp());
+  runApp(SpotFinderApp());
 }
 
-class MyApp extends StatelessWidget {
-  final controller = PageController(
-    initialPage: 1,
-  );
-
+class SpotFinderApp extends StatelessWidget {
   final TakePictureScreen takePictureScreen =
       TakePictureScreen(camera: CameraService.instance.getCamera());
   final SpotList spotListScreen = SpotList();
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: PageView(
-        controller: this.controller,
-        children: [
-          this.takePictureScreen,
-          this.spotListScreen,
-        ],
+    return GestureDetector(
+      onTap: () {
+        WidgetsBinding.instance.focusManager.primaryFocus?.unfocus();
+      },
+      child: MaterialApp(
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: CreateAccount(), // TODO: Logique
       ),
     );
   }
