aea97c403be9c9d17a90afe0873c02665c91a244
Rémy LAVERGNE <lavergne.remy@gmail.com>
2020-11-14 23:42:49 +0100

    animate camera screen push

--- lib/screens/feed.dart
+++ lib/screens/feed.dart
@@ -1,3 +1,4 @@
+import 'package:camera/new/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:geolocator/geolocator.dart';
@@ -160,14 +161,32 @@ class _FeedState extends State<Feed> with SingleTickerProviderStateMixin {
         context: context);
   }
 
+  Route _animateCreationSpotScreen(Position position) {
+    return PageRouteBuilder(
+      pageBuilder: (context, animation, secondaryAnimation) =>
+          TakePictureScreen(
+        position: position,
+        camera: CameraHelper.instance.getCamera(),
+      ),
+      transitionsBuilder: (context, animation, secondaryAnimation, child) {
+        var begin = Offset(0.0, 1.0);
+        var end = Offset.zero;
+        var curve = Curves.ease;
+
+        var tween =
+            Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+
+        return SlideTransition(
+          position: animation.drive(tween),
+          child: child,
+        );
+      },
+    );
+  }
+
   void getCurrentPositionAndNavigate(BuildContext context) {
     GeolocationHelper.instance.getCurrentPosition().then((Position position) =>
-        Navigator.push(
-            context,
-            MaterialPageRoute(
-                builder: (context) => TakePictureScreen(
-                    position: position,
-                    camera: CameraHelper.instance.getCamera()))));
+        Navigator.push(context, this._animateCreationSpotScreen(position)));
   }
 
   Widget _gridCloseSpotMOCK() {
