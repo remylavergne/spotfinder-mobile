ba0b4c43da5d7cb8d8e917fecfdf7aab67730218
Rémy LAVERGNE <lavergne.remy@gmail.com>
2020-11-16 08:37:26 +0100

    fix: mock

--- lib/models/mocks/spot.mocks.dart
+++ lib/models/mocks/spot.mocks.dart
@@ -2,19 +2,7 @@ import 'package:spotfinder/models/spot.model.dart';
 
 class SpotMocks {
   static Spot getSpot() {
-    return Spot(
-        'UUID GENERATE',
-        'Ce spot est magnifique, les rampes sont géniales, et tout est en place pour profiter au maximum ! ',
-        'Hipnotic Cable Park',
-        'Rue du lac, 64 4712 Neupré',
-        'France',
-        12,
-        45565454,
-        55454566,
-        1602009832,
-        1602009832,
-        true,
-        'UUID RIDER');
+    return null;
   }
 
   static List<Spot> getSpots() {
