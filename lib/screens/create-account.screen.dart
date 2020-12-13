16d8740d927726393af6418fded0c19f9c6a330b
Rémy LAVERGNE <lavergne.remy@gmail.com>
2020-12-13 19:52:00 +0100

    throttle buttons

--- lib/screens/create-account.screen.dart
+++ lib/screens/create-account.screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:spotfinder/generated/l10n.dart';
+import 'package:spotfinder/helpers/throttling.dart';
 import 'package:spotfinder/models/dto/login-infos.dto.dart';
 import 'package:spotfinder/repositories/repository.dart';
 import 'package:spotfinder/screens/display-clear-password.screen.dart';
@@ -19,6 +20,9 @@ class CreateAccountScreen extends StatefulWidget {
 class _CreateAccountState extends State<CreateAccountScreen> {
   final controller = PageController(initialPage: 0);
   final _formKey = GlobalKey<FormState>();
+  Throttling createSpotThrottling = Throttling(Duration(seconds: 5));
+  bool creationError = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,9 +30,10 @@ class _CreateAccountState extends State<CreateAccountScreen> {
         children: [
           this._viewPager(),
           Container(
-              margin: EdgeInsets.only(top: 35.0),
-              alignment: Alignment.topCenter,
-              child: ApplicationTitle(title: S.current.spotfinder, size: 130.0)),
+            margin: EdgeInsets.only(top: 35.0),
+            alignment: Alignment.topCenter,
+            child: ApplicationTitle(title: S.current.spotfinder, size: 130.0),
+          ),
           Positioned.fill(child: this._form(context)),
         ],
       ),
@@ -50,101 +55,107 @@ class _CreateAccountState extends State<CreateAccountScreen> {
 
   Widget _form(BuildContext context) {
     final usernameController = TextEditingController();
-    bool creationError = false;
+
     return Container(
       margin: EdgeInsets.only(
         left: 16.0,
         right: 16.0,
       ),
       child: Form(
-          key: this._formKey,
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: <Widget>[
-                Container(
-                  margin: EdgeInsets.only(bottom: 16.0),
-                  child: TextFormField(
-                    controller: usernameController,
-                    decoration: InputDecoration(
-                      hintStyle: TextStyle(
-                        color: Color(0xFF989898),
-                        fontSize: 18.0,
-                      ),
-                      hintText: S.current.chooseYourUsername,
-                      fillColor: Colors.white,
-                      filled: true,
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(4.0),
-                        borderSide: BorderSide.none,
-                      ),
-                    ),
-                    validator: (value) {
-                      if (value.isEmpty) {
-                        return S.current.pleaseEnterUsername;
-                      } else if (creationError) {
-                        creationError = false;
-                        return S.current.usernameAlreadyExists;
-                      } else {
-                        return null;
-                      }
-                    },
+        key: this._formKey,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.only(bottom: 16.0),
+              child: TextFormField(
+                controller: usernameController,
+                decoration: InputDecoration(
+                  hintStyle: TextStyle(
+                    color: Color(0xFF989898),
+                    fontSize: 18.0,
+                  ),
+                  hintText: S.current.chooseYourUsername,
+                  fillColor: Colors.white,
+                  filled: true,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(4.0),
+                    borderSide: BorderSide.none,
                   ),
                 ),
-                Container(
-                  width: double.infinity,
-                  child: FlatButton(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(4.0),
-                    ),
-                    color: Color(0xFF276FBF),
-                    textColor: Colors.white,
-                    height: 56.0,
-                    onPressed: () async {
-                      if (_formKey.currentState.validate()) {
-                        LoginInfos infos = await Repository()
-                            .createAccount(usernameController.text.trim());
-                        if (infos != null) {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => ClearPasswordScreen(
-                                  clearPassword: infos.password),
-                            ),
-                          );
-                        } else {
-                          creationError = true;
-                          _formKey.currentState.validate();
-                        }
-                      }
-                    },
-                    child: Text(
-                      S.current.next,
-                      style: TextStyle(
-                          fontSize: 26.0, fontWeight: FontWeight.bold),
-                    ),
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return S.current.pleaseEnterUsername;
+                  } else if (this.creationError) {
+                    this.creationError = false;
+                    return S.current.usernameAlreadyExists;
+                  } else {
+                    return null;
+                  }
+                },
+              ),
+            ),
+            Container(
+              width: double.infinity,
+              child: FlatButton(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(4.0),
+                ),
+                color: Color(0xFF276FBF),
+                textColor: Colors.white,
+                height: 56.0,
+                onPressed: () async {
+                  if (_formKey.currentState.validate()) {
+                    this.createSpotThrottling.throttle(() => this
+                        ._createAccountProcess(
+                            usernameController.text, context));
+                  }
+                },
+                child: Text(
+                  S.current.next,
+                  style: TextStyle(fontSize: 26.0, fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(bottom: 10.0),
+              child: FlatButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => RetrieveAccountScreen()),
+                  );
+                },
+                child: Text(
+                  S.current.login,
+                  style: TextStyle(
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
                   ),
                 ),
-                Container(
-                  margin: EdgeInsets.only(bottom: 10.0),
-                  child: FlatButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => RetrieveAccountScreen()),
-                      );
-                    },
-                    child: Text(
-                      S.current.login,
-                      style: TextStyle(
-                        fontSize: 16.0,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
-                      ),
-                    ),
-                  ),
-                ),
-              ])),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
+  }
+
+  void _createAccountProcess(String username, BuildContext context) async {
+    LoginInfos infos = await Repository().createAccount(username.trim());
+    if (infos != null) {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) =>
+              ClearPasswordScreen(clearPassword: infos.password),
+        ),
+      );
+    } else {
+      this.creationError = true;
+      _formKey.currentState.validate();
+    }
   }
 }
--- lib/screens/retrieve-account.screen.dart
+++ lib/screens/retrieve-account.screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:spotfinder/generated/l10n.dart';
+import 'package:spotfinder/helpers/throttling.dart';
 import 'package:spotfinder/repositories/repository.dart';
 import 'package:spotfinder/screens/feed.screen.dart';
 import 'package:spotfinder/widgets/application-title.dart';
@@ -16,10 +17,11 @@ class RetrieveAccountScreen extends StatefulWidget {
 class _RetrieveAccount extends State<RetrieveAccountScreen> {
   PageController controller = PageController(initialPage: 0);
   final _formKey = GlobalKey<FormState>();
-
+  Throttling loginThrottle = Throttling(Duration(seconds: 5));
   bool creationError = false;
   TextEditingController usernameCtrl = TextEditingController();
   TextEditingController passwordCtrl = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -57,131 +59,143 @@ class _RetrieveAccount extends State<RetrieveAccountScreen> {
         right: 16.0,
       ),
       child: Form(
-          key: this._formKey,
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: <Widget>[
-                // Username
-                Container(
-                  margin: EdgeInsets.only(bottom: 16.0),
-                  child: TextFormField(
-                    controller: this.usernameCtrl,
-                    decoration: InputDecoration(
-                      hintStyle: TextStyle(
-                        color: Color(0xFF989898),
-                        fontSize: 18.0,
-                      ),
-                      hintText: S.current.usernameHint,
-                      fillColor: Colors.white,
-                      filled: true,
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(4.0),
-                        borderSide: BorderSide.none,
-                      ),
-                    ),
-                    validator: (value) {
-                      if (value.isEmpty) {
-                        return S.current.pleaseEnterUsername;
-                      } else if (creationError) {
-                        creationError = false;
-                        return S.current.loginError;
-                      } else {
-                        return null;
-                      }
-                    },
+        key: this._formKey,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: <Widget>[
+            // Username
+            Container(
+              margin: EdgeInsets.only(bottom: 16.0),
+              child: TextFormField(
+                controller: this.usernameCtrl,
+                decoration: InputDecoration(
+                  hintStyle: TextStyle(
+                    color: Color(0xFF989898),
+                    fontSize: 18.0,
+                  ),
+                  hintText: S.current.usernameHint,
+                  fillColor: Colors.white,
+                  filled: true,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(4.0),
+                    borderSide: BorderSide.none,
                   ),
                 ),
-                Container(
-                  margin: EdgeInsets.only(bottom: 16.0),
-                  child: TextFormField(
-                    controller: this.passwordCtrl,
-                    decoration: InputDecoration(
-                      hintStyle: TextStyle(
-                        color: Color(0xFF989898),
-                        fontSize: 18.0,
-                      ),
-                      hintText: S.current.passwordHint,
-                      fillColor: Colors.white,
-                      filled: true,
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(4.0),
-                        borderSide: BorderSide.none,
-                      ),
-                    ),
-                    validator: (value) {
-                      if (value.isEmpty) {
-                        return S.current.passwordError;
-                      } else {
-                        return null;
-                      }
-                    },
-                    obscureText: true,
-                    enableSuggestions: false,
-                    autocorrect: false,
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return S.current.pleaseEnterUsername;
+                  } else if (this.creationError) {
+                    this.creationError = false;
+                    return S.current.loginError;
+                  } else {
+                    return null;
+                  }
+                },
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(bottom: 16.0),
+              child: TextFormField(
+                controller: this.passwordCtrl,
+                decoration: InputDecoration(
+                  hintStyle: TextStyle(
+                    color: Color(0xFF989898),
+                    fontSize: 18.0,
+                  ),
+                  hintText: S.current.passwordHint,
+                  fillColor: Colors.white,
+                  filled: true,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(4.0),
+                    borderSide: BorderSide.none,
                   ),
                 ),
-                Container(
-                  width: double.infinity,
-                  margin: EdgeInsets.only(bottom: 55.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Container(
-                        width: 180.0,
-                        child: FlatButton(
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(4.0),
-                          ),
-                          color: Color(0xFF011627),
-                          textColor: Colors.white,
-                          height: 56.0,
-                          onPressed: () {
-                            Navigator.pop(context);
-                          },
-                          child: Text(
-                            S.current.back,
-                            style: TextStyle(
-                                fontSize: 26.0, fontWeight: FontWeight.bold),
-                          ),
-                        ),
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return S.current.passwordError;
+                  } else {
+                    return null;
+                  }
+                },
+                obscureText: true,
+                enableSuggestions: false,
+                autocorrect: false,
+              ),
+            ),
+            Container(
+              width: double.infinity,
+              margin: EdgeInsets.only(bottom: 55.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    width: 180.0,
+                    child: FlatButton(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(4.0),
                       ),
-                      Container(
-                        width: 180.0,
-                        child: FlatButton(
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(4.0),
-                          ),
-                          color: Color(0xFF276FBF),
-                          textColor: Colors.white,
-                          height: 56.0,
-                          onPressed: () async {
-                            if (_formKey.currentState.validate()) {
-                              bool success = await Repository()
-                                  .connectUserByCredentials(
-                                      this.usernameCtrl.text.trim(),
-                                      this.passwordCtrl.text.trim());
-                              if (success) {
-                                Navigator.push(
+                      color: Color(0xFF011627),
+                      textColor: Colors.white,
+                      height: 56.0,
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      child: Text(
+                        S.current.back,
+                        style: TextStyle(
+                            fontSize: 26.0, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: 180.0,
+                    child: FlatButton(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(4.0),
+                      ),
+                      color: Color(0xFF276FBF),
+                      textColor: Colors.white,
+                      height: 56.0,
+                      onPressed: () async {
+                        if (_formKey.currentState.validate()) {
+                          this.loginThrottle.throttle(
+                                () => this._loginUserProcess(
                                     context,
-                                    MaterialPageRoute(
-                                        builder: (context) => FeedScreen()));
-                              } else {
-                                creationError = true;
-                                _formKey.currentState.validate();
-                              }
-                            }
-                          },
-                          child: Text(
-                            S.current.next,
-                            style: TextStyle(
-                                fontSize: 26.0, fontWeight: FontWeight.bold),
-                          ),
-                        ),
+                                    this.usernameCtrl.text,
+                                    this.passwordCtrl.text),
+                              );
+                        }
+                      },
+                      child: Text(
+                        S.current.next,
+                        style: TextStyle(
+                            fontSize: 26.0, fontWeight: FontWeight.bold),
                       ),
-                    ],
+                    ),
                   ),
-                ),
-              ])),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
+  }
+
+  void _loginUserProcess(
+      BuildContext context, String username, String password) async {
+    bool success = await Repository()
+        .connectUserByCredentials(username.trim(), password.trim());
+    if (success) {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => FeedScreen(),
+        ),
+      );
+    } else {
+      this.creationError = true;
+      _formKey.currentState.validate();
+    }
   }
 }
