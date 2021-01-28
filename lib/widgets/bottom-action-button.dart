e60aeeec7ceaca84ba272d3b00ba5dd504fb08bf
Rémy LAVERGNE <lavergne.remy@gmail.com>
2021-01-28 21:27:18 +0100

    bottom button action widget

---
+++ lib/widgets/bottom-action-button.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class BottomActionButton extends StatelessWidget {
+  final BuildContext parentContext;
+  final String text;
+  final VoidCallback onTap;
+
+  BottomActionButton({
+    Key key,
+    @required this.parentContext,
+    @required this.text,
+    @required this.onTap,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    MediaQueryData mediaQueryData = MediaQuery.of(parentContext);
+
+    return Container(
+      alignment: Alignment.bottomCenter,
+      color: Color(0xFF011627),
+      child: SizedBox(
+        width: double.maxFinite,
+        child: FlatButton(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(4.0),
+          ),
+          color: Color(0xFF011627),
+          textColor: Colors.white,
+          height: 56.0 + mediaQueryData.padding.bottom,
+          onPressed: () {
+            this.onTap();
+          },
+          child: Text(
+            this.text,
+            style: TextStyle(fontSize: 26.0, fontWeight: FontWeight.bold),
+          ),
+        ),
+      ),
+    );
+  }
+}
