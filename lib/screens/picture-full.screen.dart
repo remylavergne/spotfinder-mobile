b5bd2799a8d6956fe6f227c25b3d8983c0858e38
Rémy LAVERGNE <lavergne.remy@gmail.com>
2021-02-05 20:40:15 +0100

    informations animation

--- lib/screens/picture-full.screen.dart
+++ lib/screens/picture-full.screen.dart
@@ -7,14 +7,33 @@ import 'package:spotfinder/constants.dart';
 import 'package:spotfinder/generated/l10n.dart';
 import 'package:spotfinder/helpers/shared-preferences.helper.dart';
 import 'package:spotfinder/models/picture.model.dart';
+import 'package:spotfinder/models/user.model.dart';
+import 'package:spotfinder/repositories/repository.dart';
 
-class PictureFullScreen extends StatelessWidget {
+class PictureFullScreen extends StatefulWidget {
   static String route = '/picture-fullscreen';
   final Picture picture;
   PictureFullScreen({Key key, @required this.picture}) : super(key: key);
 
   @override
+  _PictureFullScreenState createState() => _PictureFullScreenState();
+}
+
+class _PictureFullScreenState extends State<PictureFullScreen> {
+  Future<User> _user;
+  bool _visible = true;
+
+  @override
+  void initState() {
+    super.initState();
+    this._user = Repository().getUserById(this.widget.picture.userId);
+  }
+
+  @override
   Widget build(BuildContext context) {
+    MediaQueryData mediaQueryData = MediaQuery.of(context);
+    double informationsContainerHeight = 56.0 + mediaQueryData.padding.bottom;
+
     return Scaffold(
       appBar: AppBar(
         title: Text(S.current.photoTitle),
@@ -28,13 +47,20 @@ class PictureFullScreen extends StatelessWidget {
               builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
                 if (snapshot.hasData) {
                   String token = snapshot.data;
-                  return SizedBox.expand(
-                    child: Image.network(
-                      '${Constants.getBaseApi()}/picture/id/${picture.id}',
-                      headers: {
-                        HttpHeaders.authorizationHeader: 'Bearer $token'
-                      },
-                      fit: BoxFit.cover,
+                  return GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        _visible = !_visible;
+                      });
+                    },
+                    child: SizedBox.expand(
+                      child: Image.network(
+                        '${Constants.getBaseApi()}/picture/id/${widget.picture.id}',
+                        headers: {
+                          HttpHeaders.authorizationHeader: 'Bearer $token'
+                        },
+                        fit: BoxFit.cover,
+                      ),
                     ),
                   );
                 } else {
@@ -46,47 +72,65 @@ class PictureFullScreen extends StatelessWidget {
                 }
               },
             ),
-            Align(
-              alignment: Alignment.bottomCenter,
-              child: Container(
-                width: double.maxFinite,
-                height: 80.0,
-                color: Color(0x66011627),
-                child: Column(
-                  children: [
-                    GestureDetector(
-                      onTap: () {
-                        // TODO: Open profile
-                        debugPrint('Open profile');
-                      },
+            FutureBuilder<User>(
+              future: this._user,
+              builder: (BuildContext context, AsyncSnapshot<User> snapshot) {
+                if (snapshot.hasData) {
+                  User user = snapshot.data;
+
+                  return AnimatedOpacity(
+                    opacity: _visible ? 1.0 : 0.0,
+                    duration: Duration(milliseconds: 400),
+                    child: Align(
+                      alignment: Alignment.bottomCenter,
                       child: Container(
-                        padding: const EdgeInsets.only(
-                          top: 8.0,
-                          left: 8.0,
-                        ),
                         width: double.maxFinite,
-                        child: Text(
-                          'Rémy Lavergne', // TODO: Adapter
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                          ),
+                        height: informationsContainerHeight,
+                        color: Color(0x88011627),
+                        child: Column(
+                          children: [
+                            GestureDetector(
+                              onTap: () {
+                                // TODO: Open profile // Check if not already opened
+                                debugPrint('Open profile');
+                              },
+                              child: Container(
+                                padding: const EdgeInsets.only(
+                                  top: 8.0,
+                                  left: 8.0,
+                                ),
+                                width: double.maxFinite,
+                                child: Text(
+                                  user.username,
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Padding(
+                              padding:
+                                  const EdgeInsets.only(top: 4.0, left: 8.0),
+                              child: Container(
+                                width: double.maxFinite,
+                                child: Text(
+                                  this._formatDate(
+                                      this.widget.picture.createdAt),
+                                  style: TextStyle(
+                                      color: Colors.grey, fontSize: 10.0),
+                                ),
+                              ),
+                            ),
+                          ],
                         ),
                       ),
                     ),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 4.0, left: 8.0),
-                      child: Container(
-                        width: double.maxFinite,
-                        child: Text(
-                          this._formatDate(this.picture.createdAt),
-                          style: TextStyle(color: Colors.grey, fontSize: 10.0),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
+                  );
+                } else {
+                  return Container();
+                }
+              },
             ),
           ],
         ),
