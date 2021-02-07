c6c438b929f66a128009e296235f31fa9bbc2f92
Rémy LAVERGNE <lavergne.remy@gmail.com>
2021-02-07 15:48:20 +0100

    full screen loader more options

--- lib/widgets/fullscreen-loader.dart
+++ lib/widgets/fullscreen-loader.dart
@@ -3,8 +3,10 @@ import 'package:flutter/widgets.dart';
 
 class FullscreenLoader extends StatefulWidget {
   final String message;
+  final bool loader;
 
-  const FullscreenLoader({Key key, this.message}) : super(key: key);
+  const FullscreenLoader({Key key, this.loader = true, this.message = ''})
+      : super(key: key);
 
   @override
   _FullscreenLoaderState createState() => _FullscreenLoaderState();
@@ -16,8 +18,31 @@ class _FullscreenLoaderState extends State<FullscreenLoader> {
     return SizedBox.expand(
       child: Container(
         color: Color(0x66011627),
-        child: Center(
-          child: CircularProgressIndicator(),
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Visibility(
+              visible: this.widget.loader,
+              child: CircularProgressIndicator(),
+            ),
+            Visibility(
+              visible: this.widget.message.isNotEmpty,
+              child: Container(
+                margin: this.widget.loader
+                    ? EdgeInsets.only(top: 16.0)
+                    : EdgeInsets.only(top: 0.0),
+                child: Text(
+                  this.widget.message,
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            )
+          ],
         ),
       ),
     );
